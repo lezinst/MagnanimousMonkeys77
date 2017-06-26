0aed5faaed2fdfab23057e712dbaac5cc9db6b03
Rob <robstlezin@gmail.com>
2017-06-26 15:43:59 -0700

    added stateless waiting component

---
+++ react-client/src/components/Waiting.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Waiting = () => (
+ <div className="row">
+   <div className="col-xs-12 jumbotron"><p className="text-center">waiting...</p></div>
+ </div>
+)
+
+export default Waiting;
