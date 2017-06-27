35ad1ef1ad272b74f6e1fb408aaee272851cc1cb
Shyan Kashani <shyankashani1@gmail.com>
2017-06-27 16:24:54 -0700

    MySQL database schema complete

--- schema.sql
+++ schema.sql
@@ -1,16 +1,63 @@
-DROP DATABASE IF EXISTS test;
+DROP DATABASE IF EXISTS thumbscheck;
 
-CREATE DATABASE test;
+CREATE DATABASE IF NOT EXISTS thumbscheck;
 
-USE test;
+USE thumbscheck;
 
-CREATE TABLE items (
-  id int NOT NULL AUTO_INCREMENT,
-  quantity integer NOT NULL,
-  description varchar(50) NOT NULL,
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30)  NOT NULL,
+  last_name VARCHAR(30)  NOT NULL,
+  gmail VARCHAR(30) NOT NULL,
+  user_type ENUM("STUDENT", "INSTRUCTOR") NOT NULL,
   PRIMARY KEY (ID)
 );
 
+CREATE TABLE thumbs (
+  id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
+  question_id INT NOT NULL,
+  thumb_value INT NOT NULL,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE questions (
+  id INT NOT NULL AUTO_INCREMENT,
+  lecture_id INT NOT NULL,
+  average_thumb_question DEC NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+CREATE TABLE lectures (
+  id INT NOT NULL AUTO_INCREMENT,
+  average_thumb_lecture DEC NOT NULL,
+  PRIMARY KEY (ID)
+);
+
+/*
+ *
+ */
+
+INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Rob", "St. Lezin", "robstlezin@gmail.com", "STUDENT");
+INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Shyan", "Kashani", "shyan.kashani@gmail.com", "STUDENT");
+INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Mike", "Clausen", "michaeljclausen@gmail.com", "INSTRUCTOR");
+
+
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+ *    mysql -u root < thumbs.sql
+ *  to create the database and the tables.
+ */
+
+ /*
+  *  Thumb values will be quantified between 1 and 5
+  *  and averages will hold values between 1 and 5 as well
+  *
+  */
+
+
+/*
+ *
+ * CLEARDB_DATABASE_URL
+ * mysql://be6789ba34707e:02c8f71e@us-cdbr-iron-east-03.cleardb.net/heroku_57eb1e9aa24d7a7?reconnect=true
+ */
