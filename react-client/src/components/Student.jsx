5c61cd94d981303d9c2b976d04593fe6b888b1a7
Shyan Kashani <shyankashani1@gmail.com>
2017-06-26 17:10:41 -0700

    Status and waitingFor added to Student component state

--- react-client/src/components/Student.jsx
+++ react-client/src/components/Student.jsx
@@ -3,13 +3,19 @@ import React from 'react';
 class Student extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: 'waiting',
+      waitingFor: 'lecture'
+    };
   }
 
   render () {
     return (
       <div>
         <h1>Student Component</h1>
+        {this.state.status === 'waiting'
+        ? <Waiting waitingFor={this.status.waitingFor} />
+        : <ThumbInput />}
       </div>
     )
   }
---
+++ react-client/src/components/ThumbInput.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ThumbInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render () {
+    return (
+      <div className="row">
+        <div className="col-xs-10">
+          <h1>ThumbInput Component</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ThumbInput;
