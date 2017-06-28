7f51c2b5e3e5e8431eb8dbc1e6f616f8ed5a1ca6
Shyan Kashani <shyankashani1@gmail.com>
2017-06-28 13:41:21 -0700

    Incorporated Countdown into ThumbInput

--- react-client/src/components/ThumbInput.jsx
+++ react-client/src/components/ThumbInput.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ThumbVisualization from './ThumbVisualization.jsx';
+import Countdown from './Countdown.jsx';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
@@ -20,13 +21,18 @@ class ThumbInput extends React.Component {
   render () {
     return (
       <div className="row">
-        <div className="col-xs-5" style={{height: 200}}>
-          <Slider vertical min={0} max={4} marks={{0:'Down', 1:'–', 2:'Middle', 3:'–', 4:'Up'}} step={null} onChange={this.changeThumbRotation.bind(this)} defaultValue={2} />
+        <div className="row">
+          <div className="col-xs-5" style={{height: 200}}>
+            <Slider vertical min={0} max={4} marks={{0:'Down', 1:'–', 2:'Middle', 3:'–', 4:'Up'}} step={null} onChange={this.changeThumbRotation.bind(this)} defaultValue={2} />
+          </div>
+          <div className="col-xs-5">
+            <ThumbVisualization thumbValue={this.state.thumbValue} />
+          </div>
         </div>
-        <div className="col-xs-5">
-          <ThumbVisualization thumbValue={this.state.thumbValue} />
+        <div className="row">
+          <Countdown />
         </div>
-      </div>
+    </div>
     )
   }
 }
