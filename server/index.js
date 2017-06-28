bb8fda8cf4f30044fac172b0ea1a8d6612dd1d4f
Michael Clausen <michaeljclausen@gmail.com>
2017-06-27 19:16:04 -0700

    Returns user_type from /login get request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var axios = require('axios');
-var GoogleAuth = require('google-auth-library');
-
-var auth = new GoogleAuth;
-var client = new auth.OAuth2('430160456638-mmtpqlu3h8t0nkum0tlo167d492gvbmf.apps.googleusercontent.com', '', '');
+var db = require('../database-mysql');
+var google = require('./middleware/googleAuth.js');
 
 var items = require('../database-mysql');
 
@@ -15,16 +13,13 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/../react-client/dist'));
 
 app.get('/login', (req, res) => {
-  client.verifyIdToken(
-    req.query.tokenId,
-    '430160456638-mmtpqlu3h8t0nkum0tlo167d492gvbmf.apps.googleusercontent.com',
-    function(e, login) {
-      console.log(login);
-      var payload = login.getPayload();
-      var userid = payload['email'];
-      console.log(userid);
-    }
-  );
+  google.verifyToken(req.query.tokenId, '430160456638-mmtpqlu3h8t0nkum0tlo167d492gvbmf.apps.googleusercontent.com')
+  .then(gmail => {
+    db.getUserType(gmail)
+    .then(result => {
+     res.status(200).send(result);
+    })
+  })
 })
 
 app.listen(port, function() {
