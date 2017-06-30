4685a0fb3f15c66a74017bb7c9c2caca6c47c7d8
Michael Clausen <michaeljclausen@gmail.com>
2017-06-30 12:22:55 -0700

    Bugfix for sockets

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ var google = require('./middleware/googleAuth.js');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var data = require('./middleware/thumbsData.js');
+//var data = require('./middleware/thumbsData.js');
 
 const port = process.env.PORT || 3000;
 
@@ -44,7 +44,7 @@ app.post('/checkthumbs', (req, res) => {
   db.createNewQuestion(lecture)
   .then(results => {
     questionId = results.insertId;
-    thumbs = new data.ThumbsData(lectureId, questionId);
+    thumbs = new ThumbsData(lectureId, questionId);
     //Emit the new question to students here
     io.emit('checkingThumbs', { questionId: questionId });
     //send the response to the teacher
@@ -71,15 +71,61 @@ io.on('connection', function (socket) {
   //recieve the thumb value from the student
   socket.on('thumbValue', data => {
     if (!thumbs.hasStudent(socket.username)) {
-      let student = new data.Student(socket.username, socket.id);
+      let student = new Student(socket.username, socket.id);
       thumbs.addStudent(student);
     }
-    thumbs.setValueForStudent(socket.username, data.thumbValue);
-    io.sockets.connected[instructorId].emit('averageThumbValue', { value: thumbs.getAverageThumbValue() });
+    thumbs.setThumbValueForStudent(socket.username, data.thumbValue);
+    let average = thumbs.getAverageThumbValue();
+    io.sockets.connected[instructorId].emit('averageThumbValue', { averageThumbValue: average });
+    console.log(`sending averageThumbValue of ${average}`);
     console.log(`thumb value for ${socket.username} is ${data.thumbValue}`);
   })
 });
 
+class ThumbsData {
+  constructor(lectureId, questionId, instructor) {
+    this.lectureId = lectureId;
+    this.questionId = questionId;
+    this.students = {};
+    this.instructor = instructor;
+  }
+
+  //adds a student to the data structure
+  addStudent(student) {
+    this.students[student.gmail] = student;
+  }
+
+  //sets the thumb value for the student
+  setThumbValueForStudent(gmail, thumbValue) {
+    this.students[gmail].thumbValue = thumbValue;
+  }
+
+  //returns the average thumb value
+  getAverageThumbValue() {
+    let count = 0;
+    let total = 0;
+    for (let student in this.students) {
+      if (this.students[student].thumbValue) {
+        count++;
+        total += this.students[student].thumbValue;
+      }
+    }
+    return total / count;
+  }
+
+  //check if a student is connected
+  hasStudent(gmail) {
+    return this.students.hasOwnProperty(gmail);
+  }
+}
+
+class Student {
+  constructor(gmail, socketId) {
+    this.gmail = gmail;
+    this.socketId = socketId;
+    this.thumbValue = null;
+  }
+}
 
 
 
