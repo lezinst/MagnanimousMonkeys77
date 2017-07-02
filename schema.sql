4c7e67e15120d961efca1fee04e69bfee2738dea
Michael Clausen <michaeljclausen@gmail.com>
2017-07-02 12:58:59 -0700

    Adds email to schema for testing

--- schema.sql
+++ schema.sql
@@ -48,7 +48,7 @@ CREATE TABLE lectures (
 INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Rob", "St. Lezin", "robstlezin@gmail.com", "STUDENT");
 INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Shyan", "Kashani", "shyan.kashani@gmail.com", "STUDENT");
 INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Mike", "Clausen", "michaeljclausen@gmail.com", "INSTRUCTOR");
-INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Mike", "Clausen", "somaloftSF@gmail.com", "STUDENT");
+INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Mike", "Clausen", "themike94103@gmail.com", "STUDENT");
 INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Chris", "Aaker", "caaker.0@gmail.com", "STUDENT");
 INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Robs", "Rent", "Stlezinrent@gmail.com", "INSTRUCTOR");
 INSERT INTO users (first_name, last_name, gmail, user_type) VALUES ("Shyan", "Instructor", "shyankashani1@gmail.com", "INSTRUCTOR");
